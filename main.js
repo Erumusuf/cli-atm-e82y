fd7a84f35764ad9428da51187e2ed9125866ac98
Erumusuf <160308293+Erumusuf@users.noreply.github.com>
2024-03-28 13:43:13 -0700

    A new arrow style options move up and down added
    New Style added to improve user easy to run

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ if (userpin.pinNumber === myPin) {
         message: "Please Chose An Option",
         type: "list",
         name: "operation",
-        choices: ["cashWithdraw", "balanceInquiry", "depositCash"]
+        choices: ["cashWithdraw", new inquirer.Separator(), "balanceInquiry", new inquirer.Separator(), "depositCash", new inquirer.Separator(),]
     });
     if (operationAns.operation === "balanceInquiry") {
         console.log(chalk.magentaBright(myBalance));
@@ -34,7 +34,7 @@ if (userpin.pinNumber === myPin) {
             message: "Enter Amount to  Withdraw",
             type: "list",
             name: "amountwithdraw",
-            choices: ["10000", "5000", "2000", "1000", "Other"]
+            choices: ["10000", new inquirer.Separator(), "5000", new inquirer.Separator(), "2000", new inquirer.Separator(), "1000", new inquirer.Separator(), "Other", new inquirer.Separator()]
         });
         if (withdrawAmu.amountwithdraw === "Other") {
             let otherAmu = await inquirer.prompt({
